54e17f5f1fb0976873872a18eb107a1cceb91a0a
bdu3002022it20 <148310317+bdu3002022it20@users.noreply.github.com>
2023-10-18 13:08:40 +0530

    Add files via upload

---
+++ main (2) (3).py
@@ -0,0 +1,18 @@
+def linearSearchProduct(productList, targetProduct):
+    indices = []
+
+    for index, product in enumerate(productList):
+        if product == targetProduct:
+          indices.aooend(index)
+
+return indices
+
+
+# Example usage:
+product = ["shoes", "shirt", "pants", "shoes", "shirt", "pants"]
+target = "shoes"
+result(linearSearchProduct(products, target)
+print(result
+       
+
+       
